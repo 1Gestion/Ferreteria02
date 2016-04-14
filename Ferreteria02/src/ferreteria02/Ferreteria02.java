b659ab541bc2933175e9792def618693f545e65e
F1703 <franco170391@gmail.com>
2016-04-14 01:55:07 -0300

    editandoDesdeLaWeb
    sos puto gordillo...

--- Ferreteria02/src/ferreteria02/Ferreteria02.java
+++ Ferreteria02/src/ferreteria02/Ferreteria02.java
@@ -19,6 +19,7 @@ public class Ferreteria02 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("sos puto...");
+        System.out.println("editando desde la webGitHub ");
         
     }
     public String getNombre(){
